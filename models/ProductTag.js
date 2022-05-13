5413d2d2afe7e4522dff2d8805f7138f430fcf31
Joshua Diaz <jad24096@gmail.com>
2022-05-13 17:21:56 -0400

    committing changes.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,13 +8,13 @@ ProductTag.init(
   {
     // define columns
     id: {
-      type: Datatypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
-      type: Datatypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
